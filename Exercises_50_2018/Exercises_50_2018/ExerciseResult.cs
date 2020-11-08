2b07033a089fa95b668256161f3584543ccf20e8
tomicnikola <50-2018@ftn.edu.rs>
2020-11-09 00:23:30 +0100

    Kreirana klasa ExerciseResult

---
+++ Exercises_50_2018/Exercises_50_2018/ExerciseResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises_50_2018
+{
+    class ExerciseResult
+    {
+        private int id;
+        private string studentName;
+        private string studentIndex;
+        private int points;
+
+        public int GetSetID { get; set; }
+        public string GetSetStudentName { get; set; }
+        public string GetSetStudentIndex { get; set; }
+        public int GetSetPoints { get; set; }
+
+        public ExerciseResult (int id, string studentName, string studentIndex, int points)
+        {
+            this.id = id;
+            this.studentName = studentName;
+            this.studentIndex = studentIndex;
+            this.points = points;
+        }
+    }
+}
