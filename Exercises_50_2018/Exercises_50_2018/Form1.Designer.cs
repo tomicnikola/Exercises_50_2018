a68553105fc747a6ecc0aa7825e31c91657ff504
tomicnikola <50-2018@ftn.edu.rs>
2020-11-09 03:25:48 +0100

    Dodata metoda ToString u ExerciseResult klasi kao i izmenjene get i set metode. Takodje je keirana i glavna forma za prenos podataka iz baze podataka u listu pa zatim u listbox.

--- Exercises_50_2018/Exercises_50_2018/Form1.Designer.cs
+++ Exercises_50_2018/Exercises_50_2018/Form1.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBoxExerciseResult = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxExerciseResult
+            // 
+            this.listBoxExerciseResult.FormattingEnabled = true;
+            this.listBoxExerciseResult.ItemHeight = 15;
+            this.listBoxExerciseResult.Location = new System.Drawing.Point(114, 63);
+            this.listBoxExerciseResult.Name = "listBoxExerciseResult";
+            this.listBoxExerciseResult.Size = new System.Drawing.Size(554, 304);
+            this.listBoxExerciseResult.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(341, 398);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 453);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBoxExerciseResult);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBoxExerciseResult;
+        private System.Windows.Forms.Button button1;
     }
 }
 
